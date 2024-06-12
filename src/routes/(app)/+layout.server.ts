5fb42e9d67bc1b80f5db1301ba3482476d07803e
Jaimie Lovett <jaimielovett@live.co.uk>
2024-06-12 17:32:34 +0100

    Remove the need to log in to view the app

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,11 +1,5 @@
-import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-	const session = await event.locals.getSession();
-	if (!session) {
-		throw redirect(302, '/login');
-	}
-
 	// TODO: Return loadouts
 };
